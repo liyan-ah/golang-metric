813f495f18960d1ec8b08979bbe85bfb12fd95bf
pdliyan <pdliyan@didiglobal.com>
2024-01-05 20:48:46 +0800

    read goid.

--- golang-metric-ebpf/src/main.rs
+++ golang-metric-ebpf/src/main.rs
@@ -2,10 +2,12 @@
 #![no_main]
 
 use aya_bpf::{
+    helpers::{bpf_get_current_task, bpf_probe_read},
     macros::uprobe,
     programs::ProbeContext,
 };
 use aya_log_ebpf::info;
+use golang_metric_common::vmlinux;
 
 #[uprobe]
 pub fn golang_metric(ctx: ProbeContext) -> u32 {
@@ -17,6 +19,12 @@ pub fn golang_metric(ctx: ProbeContext) -> u32 {
 
 fn try_golang_metric(ctx: ProbeContext) -> Result<u32, u32> {
     info!(&ctx, "function newproc1 called by /home/work/bin");
+    unsafe {
+        let task = bpf_get_current_task() as *const vmlinux::task_struct;
+        let fsbase = task.thread.fsbase;
+        let goid = bpf_probe_read(fsbase - 8) as u64;
+        Ok(goid as u32)
+    };
     Ok(0)
 }
 
